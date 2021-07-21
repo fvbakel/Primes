45a554df9cf5aa1a6af2f26c2d1cec2e65d4fe49
Frank van Bakel <fvbakel@gmail.com>
2021-07-21 10:06:17 +0200

    Added 8-bit word storage for segmented

--- PrimeC/solution_98/compile.sh
+++ PrimeC/solution_98/compile.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #CC="gcc -Ofast -march=native -mtune=native -funroll-all-loops -DCOMPILE_64_BIT"
-CC="gcc -Ofast -march=native -mtune=native -funroll-all-loops" 
-for x in primes_words primes_words2 primes_bit2 primes_bit primes_char primes_int ; do
+CC="gcc -Ofast -march=native -mtune=native -funroll-all-loops -DCOMPILE_8_BIT" 
+#CC="gcc -Ofast -march=native -mtune=native -funroll-all-loops"
+for x in primes_words3 primes_words primes_words2 primes_bit2 primes_bit primes_char primes_int ; do
     $CC -o $x $x.c -lm
 done
