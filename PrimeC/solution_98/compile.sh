c91b82dfe613e3a65855442a97c197298dfe71d1
Frank van Bakel <fvbakel@gmail.com>
2021-07-22 16:19:08 +0200

    Some compile experiments

--- PrimeC/solution_98/compile.sh
+++ PrimeC/solution_98/compile.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 #CC="gcc -Ofast -march=native -mtune=native -funroll-all-loops -DCOMPILE_64_BIT"
-CC="gcc -Ofast -march=native -mtune=native -funroll-all-loops -DCOMPILE_8_BIT" 
+#CC="gcc -Ofast -march=native -mtune=native -funroll-all-loops -DCOMPILE_8_BIT" 
 #CC="gcc -Ofast -march=native -mtune=native -funroll-all-loops"
-for x in primes_words3 primes_words primes_words2 primes_bit2 primes_bit primes_char primes_int ; do
+CC="gcc -pedantic -Ofast -march=native -mtune=native -funsafe-math-optimizations -funroll-all-loops"
+for x in primes_words4 primes_words3 primes_words primes_words2 primes_bit2 primes_bit primes_char primes_int ; do
     $CC -o $x $x.c -lm
 done
