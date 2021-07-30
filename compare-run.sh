8dc8cd5588f19779c53f1e579a9fcea0f45df655
Frank van Bakel <fvbakel@gmail.com>
2021-07-30 08:30:07 +0200

    Added apple specific variant

--- compare-run.sh
+++ compare-run.sh
@@ -1,9 +1,13 @@
 #/bin/bash
 source ~/node/add-path.sh
-OUTPUT_FILE=/home/fvbakel/Documenten/primes/results/output-words-2020-07-28.csv
+OUTPUT_FILE=/home/fvbakel/Documenten/primes/results/output-multi-2020-07-29.csv
 i=0
 while [ $i -lt 303 ]
 do
-    make DIRECTORY=PrimeC/solution_98 FORMATTER=csv >>$OUTPUT_FILE
+    make DIRECTORY=PrimeC/solution_1 FORMATTER=csv >>$OUTPUT_FILE
+    make DIRECTORY=PrimeC/solution_2 FORMATTER=csv >>$OUTPUT_FILE
+    make DIRECTORY=PrimeC/solution_3 FORMATTER=csv >>$OUTPUT_FILE
+    make DIRECTORY=PrimeCython/solution_1 FORMATTER=csv >>$OUTPUT_FILE
+    make DIRECTORY=PrimeMixed/solution_1 FORMATTER=csv >>$OUTPUT_FILE
     i=$[$i+1]
 done
