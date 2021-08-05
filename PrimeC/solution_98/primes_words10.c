a6ab2562efe6eaeb65dc0dd33f617f0e8869f482
Frank van Bakel <fvbakel@gmail.com>
2021-08-05 07:23:45 +0200

    Added stripped blocks

--- PrimeC/solution_98/primes_words10.c
+++ PrimeC/solution_98/primes_words10.c
@@ -498,13 +498,9 @@ int main(int argc, char **argv) {
 
     fill_cache(limit);
 
-    while(1) {
-        set_word_block_size(limit);
+    set_word_block_size(limit);
 
-        speed = run_timed_sieve(limit,maxtime,show_result,1);
-    }
-    
-
+    speed = run_timed_sieve(limit,maxtime,show_result,1);
 
     return 0;
 }
--- PrimeC/solution_98/run.sh
+++ PrimeC/solution_98/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-for x in primes_words primes_words10 primes_words09 primes_bit2 primes_bit3; do
+for x in primes_words primes_words10 primes_words09 primes_bit2 primes_bit3 primes_bit6; do
     ./$x
 done
